8c5350085762976ff76bf3557127b2c22c4bd06e
Pallavi Ballari <pp9939546@gmail.com>
2025-05-06 19:31:55 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_c8b633c6716843decc77d07b4e06490d5f566a57',
+	    'sonar.login': 'squ_ec0355374494f37264622865c41f17405580a0eb',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
